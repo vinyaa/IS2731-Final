99f8a6f019c1bc89c572a030b1a36c891816c866
howardbai <howard.bai85@gmail.com>
2015-03-30 17:39:32 -0400

    newly updated2

--- src/java/test/UserTestMain.java
+++ src/java/test/UserTestMain.java
@@ -11,5 +11,6 @@ public class UserTestMain {
     public static void main(String[] args) {
         EmailNotifier email = new EmailNotifier();
         email.sendMail("test@test.com", "hello", "this is a test.");
+        //gagaga
     }
 }

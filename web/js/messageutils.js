42886bcc8d44dc40f681ecd439210782ca275659
Yan Ma <mayanworkmail@gmail.com>
2015-04-19 17:19:49 -0400

    add Jonathan's two en/decrypt js files, haven't been used yet.

---
+++ web/js/messageutils.js
@@ -0,0 +1,61 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function encryptMessage(){
+    
+    var message = document.getElementById("message").value;
+    
+    //recipients public key - retrieve from an element called publickey
+    var publickey = document.getElementById("publickey").value;
+    var beginning = "-----BEGIN PUBLIC KEY-----";
+    var end = "-----END PUBLIC KEY-----";
+    publickey = beginning.concat(publickey);
+    publickey = publickey.concat(end);
+
+    //calls instance of JSEncrypt and encrypts the message
+    var crypt = new JSEncrypt();
+    crypt.setPublicKey(publickey);
+    var encryptedmessage = crypt.encrypt(message);
+    
+    //changes the message to encrypted form
+    document.getElementById("message").value = encryptedmessage;
+}
+
+
+function decryptMessage(){
+    
+    //your private key - retrieved from local storage
+    var privatekey; 
+    
+    var encryptedkey;
+    var decryptedkey;
+    
+    var passcode = document.getElementById("passcode").value;
+    var message = document.getElementById("message").value;
+    
+    if (typeof(Storage) != "undefined") {
+        
+        encryptedkey = localStorage.getItem("encryptedkey");
+        decryptedkey = decryptKey(encryptedkey, passcode);
+        
+    } else {
+        document.getElementById("test").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+    
+    var beginning = "-----BEGIN RSA PRIVATE KEY-----";
+    var end = "-----END RSA PRIVATE KEY-----";
+    var private = beginning.concat(decryptedkey);
+    private = private.concat(end);
+
+    
+    //calls instance of JSEncrypt and encrypts the message
+    var crypt = new JSEncrypt();
+    crypt.setPrivateKey(private);
+    var decryptedmessage = crypt.decrypt(message);
+    
+    document.getElementById("message").value = decryptedmessage;
+      
+}
